769a243357196cf3424c6adb9f5077b54f86f103
jason <awkwardkao@gmail.com>
2024-11-21 12:19:50 +0800

    udpate

--- stop_app_dev.sh
+++ stop_app_dev.sh
@@ -2,7 +2,7 @@
 
 if [ -f fastapi_dev.pid ]; then
   echo "Stopping RTAPServer Dev server..."
-  kill $(cat fastapi_dev.pid) && rm fastapi_dev.pid
+  kill -9 $(cat fastapi_dev.pid) && rm fastapi_dev.pid
   echo "RTAPServer Dev server stopped."
 else
   echo "No PID file found. Is the server running?"
