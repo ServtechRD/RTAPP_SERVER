5f7b8dee693be3b0559d1f9ea04b90acf6698c11
jason <awkwardkao@gmail.com>
2024-11-21 12:15:15 +0800

    fix sh

--- start_app_dev.sh
+++ start_app_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Starting RTAPServer Dev server..."
-uvicorn app_dev:app --host 0.0.0.0 --port 37200 --reload &
+uvicorn app_dev:app --host 0.0.0.0 --port 37200 &
 echo $! > fastapi_dev.pid
 echo "RTAPServer Dev server started with PID $(cat fastapi_dev.pid)"
