6a7899e550e70dd682bf1f0ce091bac6063142e9
ServtechRD <testvmccenter@gmail.com>
2024-12-27 11:28:54 +0000

    update shell

--- start_app.sh
+++ start_app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Starting RTAPServer server..."
-uvicorn app:app --host 0.0.0.0 --port 37000 &
+nohup uvicorn app:app --host 0.0.0.0 --port 37000 > /dev/null 2>&1 &
 echo $! > fastapi_app.pid
 echo "RTAPServer server started with PID $(cat fastapi_app.pid)"
